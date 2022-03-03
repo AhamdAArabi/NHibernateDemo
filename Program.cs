12f1155e4a58621c4c66da8e0c367e3967151a3e
AhamdAArabi <67477585+AhamdAArabi@users.noreply.github.com>
2022-03-03 15:41:29 +0300

    Profiler
    https://www.tutorialspoint.com/nhibernate/nhibernate_profiler.htm

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using NHibernate.Cfg;
+using HibernatingRhinos.Profiler.Appender.NHibernate;
+using NHibernate.Cfg;
 using NHibernate.Dialect;
 using NHibernate.Driver;
 
@@ -14,6 +15,9 @@ namespace NHibernateDemoApp
 
         static void Main(string[] args)
         {
+            NHibernateProfilerBootstrapper.PreStart();
+
+            NHibernateProfiler.Initialize();
             var cfg = new Configuration();
 
             String DataSource = "DESKTOP-69PPK9T";
@@ -37,9 +41,9 @@ namespace NHibernateDemoApp
                 x.ConnectionString = "Data Source=DESKTOP-69PPK9T;Initial Catalog=NHibernateDemoDB;Integrated Security=True";
 
 
-
                 x.Driver<SqlClientDriver>();
                 x.Dialect<MsSql2008Dialect>();
+                x.LogSqlInConsole = true;
             });
 
             cfg.AddAssembly(Assembly.GetExecutingAssembly());
@@ -47,139 +51,26 @@ namespace NHibernateDemoApp
             var sefact = cfg.BuildSessionFactory();
 
             // Add Students Record
-            //using (var session = sefact.OpenSession())
-            //{
+            using (var session = sefact.OpenSession())
+            {
 
-            //    using (var tx = session.BeginTransaction())
-            //    {
-            //        var student1 = new Student
-            //        {
-            //            ID = 1,
-            //            FirstMidName = "Allan",
-            //            LastName = "Bommer"
-            //        };
+                using (var tx = session.BeginTransaction())
+                {
+                    Console.WriteLine("\nFetch the complete list again\n");
+                    var students = session.CreateCriteria<Student>().List<Student>();
 
-            //        var student2 = new Student
-            //        {
-            //            ID = 2,
-            //            FirstMidName = "Jerry",
-            //            LastName = "Lewis"
-            //        };
+                    foreach (var student in students)
+                    {
+                        Console.WriteLine("{0} \t{1} \t{2}", student.ID, student.FirstMidName,
+                           student.LastName);
+                    }
 
-            //        var student3 = new Student
-            //        {
-            //            ID = 3,
-            //            FirstMidName = "Ahmad",
-            //            LastName = "A Al Arabi"
-            //        };
-
-            //        var student4 = new Student
-            //        {
-            //            ID = 4,
-            //            FirstMidName = "Ali",
-            //            LastName = "Man"
-            //        };
-
-            //        session.Save(student1);
-            //        session.Save(student2);
-            //        session.Save(student3);
-            //        session.Save(student4);
-            //        tx.Commit();
-            //    }
-
-            //    Console.ReadLine();
-            //}
-
-            // Fetch Students
-            //using (var session = sefact.OpenSession())
-            //{
-
-            //    using (var tx = session.BeginTransaction())
-            //    {
-            //        var students = session.CreateCriteria<Student>().List<Student>();
-
-            //        foreach (var student in students)
-            //        {
-            //            Console.WriteLine("{0} \t{1} \t{2}",
-            //               student.ID, student.FirstMidName, student.LastName);
-            //        }
-
-            //        tx.Commit();
-            //    }
-
-            //    Console.ReadLine();
-            //}
-
-
-            //Update Students and Fetch
-            //using (var session = sefact.OpenSession())
-            //{
-
-            //    using (var tx = session.BeginTransaction())
-            //    {
-            //        var students = session.CreateCriteria<Student>().List<Student>();
-
-            //        foreach (var student in students)
-            //        {
-            //            Console.WriteLine("{0} \t{1} \t{2}", student.ID,
-            //               student.FirstMidName, student.LastName);
-            //        }
-
-            //        var stdnt = session.Get<Student>(1);
-            //        Console.WriteLine("Retrieved by ID");
-            //        Console.WriteLine("{0} \t{1} \t{2}", stdnt.ID, stdnt.FirstMidName, stdnt.LastName);
-
-            //        Console.WriteLine("Update the last name of ID = {0}", stdnt.ID);
-            //        stdnt.LastName = "Donald";
-            //        session.Update(stdnt);
-            //        Console.WriteLine("\nFetch the complete list again\n");
-
-            //        foreach (var student in students)
-            //        {
-            //            Console.WriteLine("{0} \t{1} \t{2}", student.ID,
-            //               student.FirstMidName, student.LastName);
-            //        }
-
-            //        tx.Commit();
-            //    }
-
-            //Console.ReadLine();
-            //}
-
-            //Delete Student Record
-            //using (var session = sefact.OpenSession())
-            //{
-
-            //    using (var tx = session.BeginTransaction())
-            //    {
-            //        var students = session.CreateCriteria<Student>().List<Student>();
-
-            //        foreach (var student in students)
-            //        {
-            //            Console.WriteLine("{0} \t{1} \t{2}", student.ID,
-            //               student.FirstMidName, student.LastName);
-            //        }
-
-            //        var stdnt = session.Get<Student>(1);
-            //        Console.WriteLine("Retrieved by ID");
-            //        Console.WriteLine("{0} \t{1} \t{2}", stdnt.ID, stdnt.FirstMidName, stdnt.LastName);
-
-            //        Console.WriteLine("Delete the record which has ID = {0}", stdnt.ID);
-            //        session.Delete(stdnt);
-            //        Console.WriteLine("\nFetch the complete list again\n");
-
-            //        foreach (var student in students)
-            //        {
-            //            Console.WriteLine("{0} \t{1} \t{2}", student.ID, student.FirstMidName,
-            //               student.LastName);
-            //        }
-
-            //        tx.Commit();
-            //    }
-
-            //    Console.ReadLine();
-            //}
+                    tx.Commit();
+                }
+                Console.ReadLine();
+            }
 
         }
     }
 }
+

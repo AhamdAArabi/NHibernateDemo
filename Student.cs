87d609a6d8468a09eb0d4fe37ce7b54cab0a30a3
AhamdAArabi <67477585+AhamdAArabi@users.noreply.github.com>
2022-03-07 15:34:57 +0300

    NHibernate - Data Types Mapping
    https://www.tutorialspoint.com/nhibernate/nhibernate_data_types_mapping.htm

--- Student.cs
+++ Student.cs
@@ -6,10 +6,18 @@ using System.Threading.Tasks;
 
 namespace NHibernateDemoApp
 {
-    internal class Student
-    {
-        public virtual int ID { get; set; }
-        public virtual string LastName { get; set; }
-        public virtual string FirstMidName { get; set; }
-    }
+    class Student { 
+      public virtual int ID { get; set; } 
+      public virtual string LastName { get; set; } 
+      public virtual string FirstName { get; set; } 
+      public virtual StudentAcademicStanding AcademicStanding { get; set; } 
+   } 
+   
+   public enum StudentAcademicStanding { 
+      Excellent, 
+      Good, 
+      Fair, 
+      Poor, 
+      Terrible 
+   } 
 }
